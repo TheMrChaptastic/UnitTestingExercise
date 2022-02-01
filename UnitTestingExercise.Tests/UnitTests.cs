c63c711c4431fea90c27b4768b9d57547c0488cf
TheMrChaptastic <chappy@themrchaptastic.com>
2022-02-01 16:02:46 -0600

    finished

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,104 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(1, 3, 5, 9)]
+        [InlineData(4, 4, 3, 11)]
+        [InlineData(1, 1, 0, 2)]
+        [InlineData(1, -1, 0, 0)]
+        [InlineData(-1, -1, -12, -14)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var testing = new UnitTestMethods();
             //Act
-
+            int actual = testing.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(16, 2, 14)]
+        [InlineData(5, 3, 2)]
+        [InlineData(4, 6, -2)]
+        [InlineData(1, 1, 0)]
+        [InlineData(1, -1, 2)]
+        [InlineData(-1, -1, 0)]
+        [InlineData(0, 0, 0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var testing = new UnitTestMethods();
             //Act
-
+            int actual = testing.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(16, 2, 32)]
+        [InlineData(5, 3, 15)]
+        [InlineData(4, 6, 24)]
+        [InlineData(1, 1, 1)]
+        [InlineData(1, -1, -1)]
+        [InlineData(-1, -1, 1)]
+        [InlineData(0, 0, 0)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var testing = new UnitTestMethods();
             //Act
-
+            int actual = testing.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(-16, 2, -8)]
+        [InlineData(5, 1, 5)]
+        [InlineData(12, 6, 2)]
+        [InlineData(1, 1, 1)]
+        [InlineData(1, 0, 0)]
+        [InlineData(0, -1, 0)]
+        [InlineData(0, 0, 0)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var testing = new UnitTestMethods();
             //Act
-
+            int actual = testing.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void ReturnHey()
         {
             //Arrange
-
-            //Act
-
-            //Assert
-
+            var testing = new UnitTestMethods();
+            // Act
+            string actual = testing.ReturnHey();
+            // Assert
+            Assert.Equal("HEY!", actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void IsItALetter()
         {
             //Arrange
+            var testing = new UnitTestMethods();
+            string alph = "abcdefghijklmnopqrstuvwxyz";
+            //Checking all in alph
+            foreach (char letter in alph)
+            {
+                //Act
+                bool lowercaseActual = testing.IsItALetter(letter);
+                bool uppercaseActual = testing.IsItALetter(char.ToUpper(letter));
 
-            //Act
-
-            //Assert
+                // Assert
+                Assert.True(lowercaseActual);
+                Assert.True(uppercaseActual);
+            }
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
@@ -9,18 +10,42 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
-
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
-
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
         // Create a Divide method that passes 2 integers
-
+        public int Divide(int dividend, int divisor)
+        {
+            if(divisor == 0 || dividend == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string ReturnHey()
+        {
+            return "HEY!";
+        }
 
-        
+        public bool IsItALetter(char c)
+        {
+            return char.IsLetter(c);
+        }
+
     }
 }
